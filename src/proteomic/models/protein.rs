73896e887329f17ff346dc14e0b96e1cacf843cd
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2018-08-22 13:59:31 +0200

    Read extract accession from header and store both separately

--- src/proteomic/models/protein.rs
+++ src/proteomic/models/protein.rs
@@ -1,14 +1,22 @@
+extern crate regex;
+
 pub struct Protein{
     accession: String,
+    header: String,
     aa_sequence: String
 }
 
 impl Protein{
-    pub fn new(accession: String, aa_sequence: String) -> Protein {
-        Protein{
-            accession: accession,
+    pub fn new(header: String, aa_sequence: String) -> Protein {
+        return Protein{
+            accession: Protein::extract_accession_from_header(&header),
+            header: header,
             aa_sequence: aa_sequence
         }
+    }
+
+    pub fn extract_accession_from_header(header: &String) -> String {
+        return String::from(regex::Regex::new(r"[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}").unwrap().find(header.as_str()).unwrap().as_str())
     }
 
     pub fn get_aa_sequence(&self) -> &String {
@@ -19,7 +27,11 @@ impl Protein{
         &self.accession
     }
 
+    pub fn get_header(&self) -> &String {
+        &self.accession
+    }
+
     pub fn print(&self) {
-        println!("{}\nIt's aa sequence is {} long.", self.accession, self.aa_sequence.len());
+        println!("{}\n\tAA sequence is {} long.", self.accession, self.aa_sequence.len());
     }
 }
