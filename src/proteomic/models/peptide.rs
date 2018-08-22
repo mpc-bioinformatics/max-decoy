f341799f43e3d4aa68775351c12cf5103c547c58
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2018-08-22 13:51:15 +0200

    Add some new attributes

--- src/proteomic/models/peptide.rs
+++ src/proteomic/models/peptide.rs
@@ -1,29 +1,26 @@
-use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
 
 pub struct Peptide {
-    id: i64,
-    protein_ids: HashSet<i64>,
     aa_sequence: String,
-    position_in_protein: usize,
     digest_enzym: String,
     length: usize,
+    position: usize,
+    number_of_missed_cleavages: usize
 }
 
 impl Peptide {
-    pub fn new(aa_sequence: String, position_in_protein: usize, digest_enzym: String) -> Peptide {
+    pub fn new(aa_sequence: String, digest_enzym: String, position: usize, number_of_missed_cleavages: usize) -> Peptide {
         return Peptide{
-            id: -1,
-            protein_ids: HashSet::new(),
             length: aa_sequence.len(),
             aa_sequence: aa_sequence,
-            position_in_protein: position_in_protein,
             digest_enzym: digest_enzym,
+            position: position,
+            number_of_missed_cleavages: number_of_missed_cleavages
         }
     }
 
     pub fn print(&self) {
-        println!("{} => digested with {} : found in #{}", self.aa_sequence, self.digest_enzym, self.protein_ids.len());
+        println!("{}\n\tdigested with => {}\n\tpos => {}\n\tmissed_cleavages => {}\n", self.aa_sequence, self.digest_enzym, self.position, self.number_of_missed_cleavages);
     }
 }
 
