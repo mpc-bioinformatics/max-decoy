926b650741b0e1ad32a355eeac2d5eb9a6a5e191
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2018-08-22 14:14:55 +0200

    Add trimming of current line and process the last protein

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::collections::HashSet;
 
 mod proteomic;
 
 use proteomic::utility::enzym::{DigestEnzym, Trypsin};
 use proteomic::models::protein::Protein;
-
+use proteomic::models::peptide::Peptide;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,27 +21,33 @@ fn main() {
     let fasta_file = File::open(filename).expect("fasta file not found");
     let fasta_file = BufReader::new(fasta_file);
 
-    let mut accession: String = String::new();
+    let mut header: String = String::new();
     let mut aa_sequence = String::new();
+    let trypsin: Trypsin = Trypsin::new(3, 1, 50);
+
+    let mut peptides: HashSet<Peptide> = HashSet::new();
 
     for line in fasta_file.lines() {
-        let string_line = line.unwrap();
-        // strip and replace/error whitespaces
+        // trim
+        let string_line = line.unwrap().as_mut_str().trim().to_owned()  ;
         if !string_line.starts_with(">") {
             aa_sequence.push_str(&string_line);
         } else {
-            if accession.len() > 0 {
-                let mut protein: Protein = Protein::new(accession, aa_sequence);
-                let mut trypsin: Trypsin = Trypsin::new(3, 1, 50);
-                let peptides = trypsin.digest(&mut protein);
-                for pep in peptides {
-                    pep.print();
-                }
-                //aa_sequence = String::new();
-                break;
+            if header.len() > 0 {
+                let mut protein: Protein = Protein::new(header, aa_sequence);
+                // throw error if aa sequence has whitespaces
+                peptides.extend(trypsin.digest(&mut protein));
+                // for pep in &peptides {
+                //     pep.print();
+                // }
+                aa_sequence = String::new();
             }
-            accession = string_line;
+            header = string_line;
         }
     }
-    // add last line
+    // process last protein
+    let mut protein: Protein = Protein::new(header, aa_sequence);
+    // throw error if aa sequence has whitespaces
+    peptides.extend(trypsin.digest(&mut protein));
+    println!("Peptides created: {}", peptides.len());
 }
