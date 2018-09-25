8e4befe76c1249f7fec80ba9e8d729d9ee3f45d8
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2018-09-25 13:05:26 +0200

    Save current line after each commit, to start there after restarting

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::path::Path;
 
 mod proteomic;
 use proteomic::utility::enzym::{DigestEnzym, Trypsin};
@@ -13,16 +14,59 @@ use proteomic::models::peptide::Peptide;
 
 mod tests;
 
+const START_LINE_FILE_PATH: &str = "./start_line.txt";
+
+fn start_line_file_exists() -> bool {
+    return Path::new(START_LINE_FILE_PATH).is_file();
+}
+
+fn get_start_line() -> usize {
+    if start_line_file_exists() {
+        let mut start_line_file = File::open(START_LINE_FILE_PATH).unwrap();
+        let mut contents = String::new();
+        start_line_file.read_to_string(&mut contents);
+        return match contents.as_str().parse::<usize>() {
+            Ok(value) => value,
+            Err(_err) => 0
+        }
+    }
+    return 0;
+}
+
+fn update_start_line_file(line_number: usize) -> bool {
+    if start_line_file_exists() {
+        let mut file = File::create(START_LINE_FILE_PATH).unwrap();
+        return match file.write_all(format!("{}", line_number).as_bytes()) {
+            Ok(_value) => true,
+            Err(_err) => false
+        };
+    }
+    return false;
+}
+
+fn remove_start_line_file() -> bool {
+    if start_line_file_exists() {
+        return match std::fs::remove_file(START_LINE_FILE_PATH) {
+            Ok(_value) => true,
+            Err(_err) => false
+        };
+    }
+    return false;
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let filename = &args[1];
 
-    // --snip--
-    println!("In file {}", filename);
+    println!("use fasta file {}...", filename);
 
     let fasta_file = File::open(filename).expect("fasta file not found");
     let fasta_file = BufReader::new(fasta_file);
+
+    let mut current_line: usize = 1;
+    let start_line: usize = get_start_line();
+    println!("start at line {}...", start_line);
 
     let mut header: String = String::new();
     let mut aa_sequence = String::new();
@@ -35,8 +79,12 @@ fn main() {
 
     let start_time: f64 = time::precise_time_s();
     for line in fasta_file.lines() {
+        if current_line < start_line { 
+            current_line += 1;
+            continue;
+        }
         // trim
-        let string_line = line.unwrap().as_mut_str().trim().to_owned()  ;
+        let string_line = line.unwrap().as_mut_str().trim().to_owned();
         if !string_line.starts_with(">") {
             aa_sequence.push_str(&string_line);
         } else {
@@ -51,12 +99,14 @@ fn main() {
                     overall_peptide_counter += peptides.len();
                     proteins.save();
                     peptides.save();
+                    update_start_line_file(current_line);
                     proteins.clear();
                     peptides.clear();
                 }
             }
             header = string_line;
         }
+        current_line += 1;
     }
     // process last protein
     let mut protein: Protein = Protein::new(header, aa_sequence);
@@ -66,6 +116,7 @@ fn main() {
     overall_peptide_counter += peptides.len();
     peptides.save();
     proteins.save();
+    remove_start_line_file();
     let stop_time: f64 = time::precise_time_s();
     println!("Proteins processed: {}", overall_protein_counter);
     println!("  Peptides created: {}", overall_peptide_counter);
