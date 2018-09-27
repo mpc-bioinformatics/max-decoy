7191d5d3bbae33af3e9752f80fc8eb5fb3bcb051
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2018-09-27 15:07:02 +0000

    Add function to reset/wipe the database

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,35 @@ CREATE TABLE peptides (
 
 CREATE INDEX peptide_weight_idx ON peptides (weight);
 CREATE INDEX peptide_length_idx ON peptides (length);
+
+
+CREATE OR REPLACE FUNCTION self_wipe()
+    RETURNS void
+AS $$
+    DECLARE
+    BEGIN
+        DROP TABLE IF EXISTS proteins;
+        DROP TABLE IF EXISTS peptides;
+
+        CREATE TABLE proteins (
+            id SERIAL PRIMARY KEY,
+            accession varchar(10) UNIQUE NOT NULL,
+            header text NOT NULL,
+            aa_sequence text NOT NULL
+        );
+
+        CREATE INDEX protein_accession_idx ON proteins (accession);
+
+        CREATE TABLE peptides (
+            id SERIAL PRIMARY KEY,
+            aa_sequence varchar(60) UNIQUE NOT NULL,
+            length integer NOT NULL,
+            number_of_missed_cleavages integer,
+            weight integer NOT NULL,
+            digest_enzym varchar(60) NOT NULL
+        );
+
+        CREATE INDEX peptide_weight_idx ON peptides (weight);
+        CREATE INDEX peptide_length_idx ON peptides (length);
+    END;
+$$ LANGUAGE plpgsql;
