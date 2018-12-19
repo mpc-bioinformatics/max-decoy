9a1dd06b05657c5b96d6e5e25700d099e0d298dd
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2018-12-19 15:41:52 +0100

    Implement methods to convert mass

--- src/proteomic/models/mass/mod.rs
+++ src/proteomic/models/mass/mod.rs
@@ -1,1 +1,11 @@
 pub mod neutral_loss;
+
+const MASS_CONVERT_FACTOR: f64 = 1000000.0;
+
+pub fn convert_mass_to_int(mass: f64) -> i64 {
+    return (mass * MASS_CONVERT_FACTOR) as i64;
+}
+
+pub fn convert_mass_to_float(mass: i64) -> f64 {
+    return mass as f64 / MASS_CONVERT_FACTOR;
+}
