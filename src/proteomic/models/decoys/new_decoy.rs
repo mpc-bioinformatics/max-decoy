2ae8cb5dfe2d0084978d55d4b944397039cd3873
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2019-01-02 16:06:50 +0100

    Add modification counter

--- src/proteomic/models/decoys/new_decoy.rs
+++ src/proteomic/models/decoys/new_decoy.rs
@@ -51,98 +51,15 @@ impl NewDecoy {
             | ((self.lower_weight_limit <= self.modified_weight) & (self.modified_weight <= self.upper_weight_limit));
     }
 
-    pub fn difference_to_upper_weight_limit(&self) -> i64 {
+    pub fn get_difference_weight_to_upper_weight_limit(&self) -> i64 {
         return self.upper_weight_limit - self.weight;
     }
 
-    fn push_amino_acid(&mut self, amino_acid: &AminoAcid) {
-        // after adding a new amino acid, the current has no free c-terminus anymore. so we have to clean the current c-terminus-modification
-        if self.c_terminus_modification.is_some() {
-            self.set_c_terminus_modification_to_none();
-        }
+    pub fn push_amino_acid(&mut self, amino_acid: &AminoAcid) {
         self.weight += amino_acid.get_mono_mass();
         self.modified_weight += amino_acid.get_mono_mass();
         self.aa_sequence.push(amino_acid.get_one_letter_code());
-    }
-
-    fn push_modification(&mut self, modification_option: &Option<&Modification>) {
-        match modification_option {
-            Some(modification) => {
-                // if this is the first modification at all and the for the first amino acid
-                if (self.modifications.len() == 0) & (self.get_length() == 1) {
-                    match modification.get_position() {
-                        // modification for n-terminus
-                        ModificationPosition::NTerminus => {
-                            self.modified_weight += modification.get_mono_mass();
-                            self.n_terminus_modification = Some((*modification).clone());
-                            self.number_of_modifications += 1;
-                        }
-                        // modification for anywhere
-                        ModificationPosition::Anywhere => {
-                            self.modified_weight += modification.get_mono_mass();
-                            self.modifications.push(Some((*modification).clone()));
-                            self.number_of_modifications += 1;
-                        }
-                        // else
-                        _ => self.modifications.push(None)
-                    }
-                } else {
-                    match modification.get_position() {
-                        // modification for c-terminus
-                        ModificationPosition::CTerminus => {
-                            self.modified_weight += modification.get_mono_mass();
-                            self.c_terminus_modification = Some((*modification).clone());
-                            self.number_of_modifications += 1;
-                        }
-                        // modification for anywhere
-                        ModificationPosition::Anywhere => {
-                            self.modified_weight += modification.get_mono_mass();
-                            self.modifications.push(Some((*modification).clone()));
-                            self.number_of_modifications += 1;
-                        }
-                        // else
-                        _ => self.modifications.push(None)
-                    }
-                }
-            },
-            None => self.modifications.push(None)
-        }
-    }
-
-    pub fn push_amino_acid_with_modification(&mut self, amino_acid: &AminoAcid, modification_option: &Option<&Modification>) -> Result<(), NewDecoyError> {
-        match modification_option {
-            // check if modfication matches amino acid
-            Some(modification) if amino_acid.get_one_letter_code() != modification.get_amino_acid_one_letter_code() => return Err(NewDecoyError::ModificationDoesNotMatchToAminoAcid),
-            Some(_) => (),  // else
-            None => (),
-        }
-        self.push_amino_acid(amino_acid);
-        self.push_modification(modification_option);
-        return Ok(());
-    }
-
-    fn is_amino_acid_fitting(&self, amino_acid: &AminoAcid) -> bool {
-        let temp_weight: i64 = self.weight + amino_acid.get_mono_mass();
-        return (self.lower_weight_limit <= temp_weight) & (temp_weight <= self.upper_weight_limit)
-    }
-
-    fn is_modification_fitting(&self, modification: &Modification) -> bool {
-        let temp_weight: i64 = self.modified_weight + modification.get_mono_mass();
-        return (self.lower_weight_limit <= temp_weight) & (temp_weight <= self.upper_weight_limit);
-    }
-
-    // removes the old modification and returns it
-    fn set_c_terminus_modification_to_none(&mut self) -> Option<Modification> {
-        let c_terminus_modification_option_clone: Option<Modification> = self.c_terminus_modification.clone();
-        match &self.c_terminus_modification {
-            Some(modification) => {
-                self.modified_weight -= modification.get_mono_mass();
-                self.number_of_modifications -= 1;
-            },
-            None => ()
-        };
-        self.c_terminus_modification = None;
-        return c_terminus_modification_option_clone;
+        self.modifications.push(None);
     }
 
     pub fn as_base_decoy(&self) -> BaseDecoy {
