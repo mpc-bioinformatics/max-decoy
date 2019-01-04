25c630ed205647dac6fbe92366bce318210f2108
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2019-01-04 21:51:50 +0100

    Adjust to previous changes in Persistable::QueryError

--- src/proteomic/models/amino_acids/tests/modification.rs
+++ src/proteomic/models/amino_acids/tests/modification.rs
@@ -1,5 +1,5 @@
 use proteomic::utility::database_connection::DatabaseConnection;
-use proteomic::models::persistable::{Persistable, panic_or_print_query_error};
+use proteomic::models::persistable::Persistable;
 use proteomic::models::amino_acids::modification::Modification;
 use proteomic::models::amino_acids::modification::ModificationPosition;
 
@@ -9,10 +9,7 @@ fn modification_insert() {
     Modification::delete_all(&conn);
     let mut modification_count: i64 = match Modification::get_count(&conn) {
         Ok(count) => count,
-        Err(err) => {
-            panic_or_print_query_error(&err, "proteomic::models::amino_acids::tests::modification::modification_insert at first Modification::get_count()", true);
-            return ();
-        }
+        Err(err) => panic!("proteomic::models::amino_acids::tests::modification::modification_insert at first Modification::get_count(): {}", err)
     };
     assert_eq!(modification_count, 0);
     let mut modification: Modification = Modification::new(
@@ -25,15 +22,12 @@ fn modification_insert() {
     );
     match modification.create(&conn) {
         Ok(_) => println!("Modification successfully created"),
-        Err(err) => panic_or_print_query_error(&err, "proteomic::models::amino_acids::tests::modification::modification_insert at Modification.create()", true)
+        Err(err) => panic!("proteomic::models::amino_acids::tests::modification::modification_insert at Modification.create(): {}", err)
     }
     assert!(modification.get_primary_key() > 0);
     modification_count = match Modification::get_count(&conn) {
         Ok(count) => count,
-        Err(err) => {
-            panic_or_print_query_error(&err, "proteomic::models::amino_acids::tests::modification::modification_insert at first Modification::get_count()", true);
-            return ();
-        }
+        Err(err) => panic!("proteomic::models::amino_acids::tests::modification::modification_insert at first Modification::get_count(): {}", err)
     };
     assert_eq!(modification_count, 1);
 }
