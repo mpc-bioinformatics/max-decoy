8a20a6e6854308ebad5e823445100ba26bb16b23
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2019-01-25 09:13:14 +0100

    Adapt get() to pass database connection with lifetime

--- src/proteomic/models/enzyms/mod.rs
+++ src/proteomic/models/enzyms/mod.rs
@@ -1,13 +1,13 @@
 pub mod digest_enzym;
 pub mod trypsin;
-pub mod results;
+pub mod digest_summary;
 
 use self::digest_enzym::DigestEnzym;
 use self::trypsin::Trypsin;
 
-pub fn get_enzym_by_name(enzym_name: &String, max_number_of_missed_cleavages: i16, min_peptide_length: usize, max_peptide_length: usize) -> impl DigestEnzym {
+pub fn get<'t>(enzym_name: &str, database_connection: &'t postgres::Connection, max_number_of_missed_cleavages: i16, min_peptide_length: usize, max_peptide_length: usize) -> impl DigestEnzym<'t> {
     match enzym_name.to_lowercase().as_str() {
-        "trypsin" => Trypsin::new(max_number_of_missed_cleavages, min_peptide_length, max_peptide_length),
-        _ => Trypsin::new(max_number_of_missed_cleavages, min_peptide_length, max_peptide_length)
+        "trypsin" => Trypsin::new(database_connection, max_number_of_missed_cleavages, min_peptide_length, max_peptide_length),
+        _ => Trypsin::new(database_connection, max_number_of_missed_cleavages, min_peptide_length, max_peptide_length)
     }
 }
