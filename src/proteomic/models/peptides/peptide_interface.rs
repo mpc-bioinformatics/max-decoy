ca3e7298b6749f53739fff130aff8200933eb246
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2019-01-29 15:55:19 +0100

    Add function for amino acid counting and creatign fasta entry

--- src/proteomic/models/peptides/peptide_interface.rs
+++ src/proteomic/models/peptides/peptide_interface.rs
@@ -1,12 +1,11 @@
-
-use std::string::ToString;
+use std::collections::HashMap;
+use proteomic::models::amino_acids::amino_acid::AMINO_ACIDS_FOR_COUNTING;
 
 /// Trait for accessing fields of a peptide.
 /// Peptides are all kinds of amino acids subsequences like Peptide, Decoy, ModifiedPeptides and ModifiedDecoy
 pub trait PeptideInterface {
     fn to_string(&self) -> String;
-    fn get_header(&self) -> String;
-    fn get_aa_sequence(&self) -> String;
+    fn get_aa_sequence(&self) -> &str;
     fn get_weight(&self) -> i64;
     fn get_length(&self) -> i32;
     // returns the last char of aa_sequence
@@ -19,7 +18,15 @@ pub trait PeptideInterface {
     // or '_' if idx is larger the aa_sequence
     fn get_amino_acid_at(&self, idx: usize) -> char;
 
-    fn as_fasta_entry(&self) -> String {
-        return format!("{}\n{}", self.get_header(), self.get_aa_sequence());
+    fn as_fasta_entry(header: &str, aa_sequence: &str) -> String {
+        return format!("{}\n{}", header, aa_sequence);
+    }
+
+    fn count_amino_acids(aa_sequence: &str) -> Box<HashMap<char, i16>> {
+        let mut amino_acids_counts: Box<HashMap<char, i16>> = Box::new(HashMap::new());
+        for one_letter_code in AMINO_ACIDS_FOR_COUNTING {
+            amino_acids_counts.insert(*one_letter_code, aa_sequence.matches(*one_letter_code).count() as i16);
+        }
+        return amino_acids_counts;
     }
 }
