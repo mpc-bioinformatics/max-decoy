9141e843892210224b495f29fe893bcca7188d54
Dirk Winkelhardt <dirk.winkelhardt@gmail.com>
2019-02-07 13:55:31 +0100

    Add method for calculating parts per million

--- src/proteomic/utility/mod.rs
+++ src/proteomic/utility/mod.rs
@@ -4,3 +4,7 @@ pub mod logger;
 pub mod combinations;
 pub mod decoy_generator;
 pub mod mz_ml;
+
+pub fn parts_per_million_of(value: f64, ppm: i64) -> f64 {
+    return  value / 1000000.0 * ppm as f64;
+}
